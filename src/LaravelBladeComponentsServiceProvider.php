c7836141d63934d83f7aa9fb1ec1d24b46b941d6
Kye Buffery <kyebuffery14@gmail.com>
2025-10-21 14:00:24 +0100

    main: Project re-init

--- src/LaravelBladeComponentsServiceProvider.php
+++ src/LaravelBladeComponentsServiceProvider.php
@@ -9,25 +9,25 @@ class LaravelBladeComponentsServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__ . '/components', 'laravelbladecomponents');
+        $this->loadViewsFrom(__DIR__ . '/components', 'ap-verify-components');
 
-        Blade::component('laravelbladecomponents::tab', 'tab');
-        Blade::component('laravelbladecomponents::modal', 'modal');
-        Blade::component('laravelbladecomponents::form', 'form');
-        Blade::component('laravelbladecomponents::dropdown-link', 'dropdown-link');
-        Blade::component('laravelbladecomponents::dropdown', 'dropdown');
-        Blade::component('laravelbladecomponents::button', 'button');
-        Blade::component('laravelbladecomponents::2col', '2col');
+        Blade::component('ap-verify-components::tab', 'tab');
+        Blade::component('ap-verify-components::modal', 'modal');
+        Blade::component('ap-verify-components::form', 'form');
+        Blade::component('ap-verify-components::dropdown-link', 'dropdown-link');
+        Blade::component('ap-verify-components::dropdown', 'dropdown');
+        Blade::component('ap-verify-components::button', 'button');
+        Blade::component('ap-verify-components::2col', '2col');
 
-        Blade::component('laravelbladecomponents::form.input', 'form.input');
-        Blade::component('laravelbladecomponents::form.textarea', 'form.textarea');
-        Blade::component('laravelbladecomponents::form.checkbox', 'form.checkbox');
-        Blade::component('laravelbladecomponents::form.radio', 'form.radio');
-        Blade::component('laravelbladecomponents::form.select', 'form.select');
-        Blade::component('laravelbladecomponents::form.selectoption', 'form.selectoption');
+        Blade::component('ap-verify-components::form.input', 'form.input');
+        Blade::component('ap-verify-components::form.textarea', 'form.textarea');
+        Blade::component('ap-verify-components::form.checkbox', 'form.checkbox');
+        Blade::component('ap-verify-components::form.radio', 'form.radio');
+        Blade::component('ap-verify-components::form.select', 'form.select');
+        Blade::component('ap-verify-components::form.selectoption', 'form.selectoption');
 
-        Blade::component('laravelbladecomponents::tabs.div', 'tabs.div');
-        Blade::component('laravelbladecomponents::tabs.header', 'tabs.header');
-        Blade::component('laravelbladecomponents::tabs.link', 'tabs.link');
+        Blade::component('ap-verify-components::tabs.div', 'tabs.div');
+        Blade::component('ap-verify-components::tabs.header', 'tabs.header');
+        Blade::component('ap-verify-components::tabs.link', 'tabs.link');
     }
 }
